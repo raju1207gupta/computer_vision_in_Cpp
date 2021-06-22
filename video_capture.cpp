7e1a64b773f97c92b3cbe5d6c2905c9487e29202
raju.gupta <raju.gupta@blp.co.in>
2021-06-22 19:33:53 +0530

    learn some of the image processing techniques using opencv

--- video_capture.cpp
+++ video_capture.cpp
@@ -32,18 +32,18 @@ int main(int argc, char** argv) {
         video.read(image);
         
         cv::resize(image,image,cv::Size(1280,720));
-        //cv::resize(image,image,cv::Size(),0.75,0.75,cv::INTER_AREA);
-        // cv::cvtColor(image,image_gray,cv::COLOR_BGR2GRAY);
-        // cv::GaussianBlur(image,image_blur,cv::Size(3,3),3,0);
-        // cv::Canny(image_blur,image_Canny,25,75);
+        cv::resize(image,image,cv::Size(),0.75,0.75,cv::INTER_AREA);
+        cv::cvtColor(image,image_gray,cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(image,image_blur,cv::Size(3,3),3,0);
+        cv::Canny(image_blur,image_Canny,25,75);
         
-        // cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(5,5));
-        // cv::dilate(image_Canny,image_dilate,kernel);
-        // cv::erode(image_dilate,image_erode,kernel);
+        cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(5,5));
+        cv::dilate(image_Canny,image_dilate,kernel);
+        cv::erode(image_dilate,image_erode,kernel);
 
-        //cv::Rect roi(100,100,300,250);
+        cv::Rect roi(100,100,300,250);
     
-        //imgcrop = image(roi);
+        imgcrop = image(roi);
         // if (!image.data)
         // {
         //     cout << "No image data";
@@ -51,12 +51,12 @@ int main(int argc, char** argv) {
         // }
         //cv::namedWindow("Display_output",cv::WINDOW_AUTOSIZE);
         cv::imshow("Display_output",image);
-        //cv::imshow("Display_output_image_gray",image_gray);
-        //cv::imshow("Display_output_image_blur",image_blur);
-        // cv::imshow("Display_output_image_Canny",image_Canny);
-        // cv::imshow("Display_output_image_dilation",image_dilate);
-        // cv::imshow("Display_output_image_erode",image_erode);
-        //cv::imshow("Display_output_iimgcrop",imgcrop);
+        cv::imshow("Display_output_image_gray",image_gray);
+        cv::imshow("Display_output_image_blur",image_blur);
+        cv::imshow("Display_output_image_Canny",image_Canny);
+        cv::imshow("Display_output_image_dilation",image_dilate);
+        cv::imshow("Display_output_image_erode",image_erode);
+        cv::imshow("Display_output_iimgcrop",imgcrop);
         int k = cv::waitKey(1);
         if (k =='q')
         {
